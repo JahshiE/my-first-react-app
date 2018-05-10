f1fa50d5f02257ea4caf16c9daf53d84d1be30df
Jahsh <joshua.espinas@gmail.com>
2018-05-10 13:35:00 +1200

    Using the state property of a class to change information from inside a component. Uses the 'this' keyword to dynamically access the state property. Unfinished at the moment.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Josh', age: 22 },
+      { name: 'Justin', age: 19},
+      { name: 'Janella', age: 27},
+      { name: 'Jorel', age: 31}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App. And I will continue this tomorrow.</h1>
         <p>This is the fam:</p>
-        <Person name="Josh" age="22"/>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name="Justin" age="19">My Hobbies: Basketball</Person>
         <Person name="Janella" age="27"/>
         <Person name="Jorel" age="32"/>
