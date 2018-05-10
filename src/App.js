11e0f893c83486523bdea486f32db6d68bc2360f
Jahsh <joshua.espinas@gmail.com>
2018-05-10 13:12:20 +1200

    Utilising how properties of a component are used and dynamically accessed via "props" parameter.

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, I'm a React App. And I will continue this tomorrow.</h1>
-        <p>All this commits were a test.</p>
-        <Person />
-        <Person />
-        <Person />
-        <Person />
+        <p>This is the fam:</p>
+        <Person name="Josh" age="22"/>
+        <Person name="Justin" age="19">My Hobbies: Basketball</Person>
+        <Person name="Janella" age="27"/>
+        <Person name="Jorel" age="32"/>
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
