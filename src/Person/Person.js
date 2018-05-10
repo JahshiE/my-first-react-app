2e97dd1d7fb77fee72f2835059dc104eacc19701
Jahsh <joshua.espinas@gmail.com>
2018-05-10 12:53:19 +1200

    Created a 'Person' functional component

---
+++ src/Person/Person.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Using ES6
+
+// lowercase person for the function name, usually same as file name.
+const person = () => {
+    return (
+        <p>I'm a Person!</p>
+    )
+}
+
+export default person;
