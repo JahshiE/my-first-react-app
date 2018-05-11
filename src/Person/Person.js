fdf68b0dc106a490e0f4686d589db2f5aafa5619
Jahsh <joshua.espinas@gmail.com>
2018-05-12 01:36:27 +1200

    Stylingthe the person objects as cards via an external Person.css file imported in.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import './Person.css';
+
 // Using ES6
 
 // lowercase person for the function name, usually same as file name.
 const person = (props) => {
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
