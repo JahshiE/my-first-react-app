66987d4cdd0b99f7558df21c79e153bac5027505
Jahsh <joshua.espinas@gmail.com>
2018-05-12 01:04:29 +1200

    Passing methods between componenents via props. Then passing a value for this method via method.bind or as an object { } for an arow function with the value passed.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Person from './Person/Person'
 class App extends Component {
   state = {
     persons: [
-      { name: 'Josh', age: 22 },
+      { name: 'Josh', age: 22},
       { name: 'Justin', age: 19},
       { name: 'Janella', age: 27},
       { name: 'Jorel', age: 31}
@@ -13,12 +13,12 @@ class App extends Component {
     otherState: 'some other value'
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked!');
     // DON'T DO THIS: this.state.persons[0].name = "Joshua";
     this.setState({
       persons: [
-        { name: 'Joshua', age: 22 },
+        { name: newName, age: 22 },
         { name: 'Justin Matthew', age: 19},
         { name: 'Janella', age: 27},
         { name: 'Jorel', age: 31}
@@ -31,11 +31,20 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App. And I will continue this tomorrow.</h1>
         <p>This is the fam:</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Basketball</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-        <Person name={this.state.persons[3].name} age={this.state.persons[3].age}/>
+        <button onClick={() => this.switchNameHandler('Josh!!!')}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age} 
+          click={this.switchNameHandler.bind(this, 'Josh!')} >My Hobbies: Basketball</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} />
+        <Person 
+          name={this.state.persons[3].name} 
+          age={this.state.persons[3].age} />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
