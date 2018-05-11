e1127de706e34a3d9a709be3b9d4a94070a16330
Jahsh <joshua.espinas@gmail.com>
2018-05-12 01:53:48 +1200

    Styling the switch name button inline using a style object written in JS

--- src/App.js
+++ src/App.js
@@ -38,11 +38,20 @@ class App extends Component {
   }
 
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
     return (
       <div className="App">
         <h1>Hi, I'm a React App. And I will continue this tomorrow.</h1>
         <p>This is the fam:</p>
-        <button onClick={() => this.switchNameHandler('Josh!!!')}>Switch Name</button>
+        <button 
+          style={style}
+          onClick={() => this.switchNameHandler('Josh!!!')}>Switch Name</button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age} />
